2ad0575978b8c1e15e588adee5b815c6fcd84bd2
Jayesh M <mandlik87@gmail.com>
2025-04-02 00:27:16 +0530

    implement start, stop, get process logs

--- main.go
+++ main.go
@@ -5,9 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
 	"strings"
-
-	// "os/exec"
 
 	"gopkg.in/yaml.v2"
 )
@@ -22,7 +21,16 @@ type Config struct {
 	Processes []ProcessConfig `yaml:"processes"`
 }
 
+type Process struct {
+	Cmd        *exec.Cmd
+	StdOutPipe io.ReadCloser
+	StdErrPipe io.ReadCloser
+	LogBuffer  []string
+}
+
 var config Config
+var ProcessDict = make(map[string]ProcessConfig)
+var processes = make(map[string]*Process)
 
 func main() {
 	loadConfig()
@@ -33,7 +41,8 @@ func main() {
 		fmt.Println("1. List processes")
 		fmt.Println("2. Start process")
 		fmt.Println("3. Stop process")
-		fmt.Println("4. Exit")
+		fmt.Println("4. Get logs")
+		fmt.Println("5. Exit")
 
 		choice, _ := reader.ReadString('\n')
 
@@ -47,31 +56,75 @@ func main() {
 		case "3":
 			stopProcess()
 		case "4":
+			printLogs()
+		case "5":
 			os.Exit(0)
 		default:
 			fmt.Println("Invalid choice. Please enter a number between 1 and 5.")
 		}
 	}
-	// cmd := exec.Command("cat", "main.go")
-	//
-	// stdOut, _ := cmd.StdoutPipe()
-	// stdErr, _ := cmd.StderrPipe()
-	//
-	// if err := cmd.Start(); err != nil {
-	// 	fmt.Printf("Error starting command: %v\n", err)
-	// 	return
-	// }
-	//
-	// printLogs(stdOut, "STDOUT")
-	// printLogs(stdErr, "STDERR")
+}
+
+func printLogs() {
+	fmt.Println("Enter process name to get logs:")
+	reader := bufio.NewReader(os.Stdin)
+	choice, _ := reader.ReadString('\n')
+	choice = strings.TrimSpace(choice)
+	p := processes[choice]
+
+	if len(p.LogBuffer) == 0 {
+		fmt.Println("No logs available")
+	}
+	for _, log := range p.LogBuffer {
+		fmt.Print(log)
+	}
 }
 
 func stopProcess() {
-	panic("unimplemented")
+	fmt.Println("Enter process name to kill:")
+	reader := bufio.NewReader(os.Stdin)
+	choice, _ := reader.ReadString('\n')
+	choice = strings.TrimSpace(choice)
+	p, exists := processes[choice]
+	if !exists {
+		fmt.Printf("%s process is not running\n", choice)
+	}
+	p.Cmd.Process.Kill()
+	fmt.Printf("%s process stopped", choice)
 }
 
 func startProcess() {
-	panic("unimplemented")
+	fmt.Println("Enter process name to start:")
+	reader := bufio.NewReader(os.Stdin)
+	choice, _ := reader.ReadString('\n')
+	choice = strings.TrimSpace(choice)
+	process := ProcessDict[choice]
+
+	if _, exists := processes[process.Name]; exists {
+		fmt.Printf("%s process is already running", process.Name)
+		return
+	}
+
+	cmd := exec.Command(process.Command, process.Args...)
+
+	stdOut, _ := cmd.StdoutPipe()
+	stdErr, _ := cmd.StderrPipe()
+
+	p := &Process{
+		Cmd:        cmd,
+		StdOutPipe: stdOut,
+		StdErrPipe: stdErr,
+	}
+
+	processes[process.Name] = p
+
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("Error starting command: %v\n", err)
+		return
+	}
+
+	go captureLogs(p, stdOut, "STDOUT")
+	go captureLogs(p, stdErr, "STDERR")
 }
 
 func listProcesses() {
@@ -92,21 +145,21 @@ func loadConfig() {
 		fmt.Printf("Failed to parse config file: %v\n", err)
 		os.Exit(1)
 	}
+	for _, process := range config.Processes {
+		ProcessDict[process.Name] = process
+	}
 }
 
-func printLogs(pipe io.ReadCloser, logLvl string) {
-	buffer := make([]byte, 1024)
+func captureLogs(process *Process, pipe io.ReadCloser, logLvl string) {
+	reader := bufio.NewReader(pipe)
 	for {
-		n, err := pipe.Read(buffer)
-		if n > 0 {
-			fmt.Printf("[%s] %s", logLvl, buffer[:n])
-		}
+		line, err := reader.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
-			fmt.Printf("[%s] Error reading logs: %v\n", logLvl, err)
-			break
 		}
+		logLine := fmt.Sprintf("[%s] %s", logLvl, line)
+		process.LogBuffer = append(process.LogBuffer, logLine)
 	}
 }
